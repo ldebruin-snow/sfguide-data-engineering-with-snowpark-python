85f7446fb4a6fac512bbe3b9ffca10d8719e21ce
Luke <luke.debruin@snowflake.com>
2023-03-28 13:41:05 -0400

    Developer123 - im mkaking this change for boss231

--- steps/05_fahrenheit_to_celsius_udf/app.py
+++ steps/05_fahrenheit_to_celsius_udf/app.py
@@ -12,12 +12,10 @@
 import sys
 from scipy.constants import convert_temperature
 
-##comment for testing
-
 def main(temp_f: float) -> float:
     return convert_temperature(float(temp_f), 'F', 'C')
 
-
+## comment 1
 # For local debugging
 # Be aware you may need to type-convert arguments if you add input parameters
 if __name__ == '__main__':
