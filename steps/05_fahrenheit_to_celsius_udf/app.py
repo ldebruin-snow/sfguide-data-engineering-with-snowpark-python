85890c2a1b00557fc97f476e5122a40b31c0db32
Luke <luke.debruin@snowflake.com>
2023-03-30 09:09:06 -0400

    Fix for HRS- BUG123

--- steps/05_fahrenheit_to_celsius_udf/app.py
+++ steps/05_fahrenheit_to_celsius_udf/app.py
@@ -15,7 +15,7 @@ from scipy.constants import convert_temperature
 def main(temp_f: float) -> float:
     return convert_temperature(float(temp_f), 'F', 'C')
 
-## comment 1
+##comment for demo
 # For local debugging
 # Be aware you may need to type-convert arguments if you add input parameters
 if __name__ == '__main__':
