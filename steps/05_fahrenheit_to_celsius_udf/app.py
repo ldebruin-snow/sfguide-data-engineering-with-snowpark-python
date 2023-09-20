b2d8744f302b4eaa335cced2ac3b748044bdd972
Luke <luke.debruin@snowflake.com>
2023-09-20 11:09:02 -0400

    pushing the latest changes

--- steps/05_fahrenheit_to_celsius_udf/app.py
+++ steps/05_fahrenheit_to_celsius_udf/app.py
@@ -9,15 +9,15 @@
 # SNOWFLAKE ADVANTAGE: Python UDFs (with third-party packages)
 
 import sys
-from scipy.constants import convert_temperature
 
 def main(temp_f: float) -> float:
-    return convert_temperature(float(temp_f), 'F', 'C')
+    return (float(temp_f) - 32) * (5/9)
 
 
+# For local debugging
 # Be aware you may need to type-convert arguments if you add input parameters
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         print(main(*sys.argv[1:]))  # type: ignore
     else:
-        print(main())  # type: ignore
+        print(main())
